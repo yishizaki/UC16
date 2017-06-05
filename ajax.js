4121a64e88f613d476f3f6ee4793ea209d9a9eb2
Yoko  Ishizaki <yi35143@srcs.loc>
2017-06-05 12:41:45 -0700

    Final

--- ajax.js
+++ ajax.js
@@ -1,49 +1,43 @@
-/**
- * Uses AJAX to query an internet data source for zip codes
- * @param {string} zipId The element id that has the zip code
- */
-function findShow(showId) {
+function findShow(seasonId, episodeId) {
     // First get the zip code from the HTML textbox
-    var show = document.getElementById(showId).value;
+    var season = document.getElementById(seasonId).value;
+    var episode = document.getElementById(episodeId).value;
     // Now make a HTTP request
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
-            if(this.status === 200) {
+            if (this.status === 200) {
                 // The request was successful!
-                displayPlace(this.responseText);
-            } else if (this.status === 404){
+                displaySummary(this.responseText);
+            }
+            else if (this.status === 404) {
                 // No postal code found
-                displayCast('{ "country" : "none" }');
-            } else {
+                displaySummary("none");
+            }
+            else {
                 console.log("We have a problem...server responded with code: " + this.status);
             }
-        } else {
+        }
+        else {
             // Waiting for a response...
         }
     };
     // Notice how the URL is appended with the zip code
-    var url = "http://api.tvmaze.com/shows/:id/cast" + show;
+    var url = "http://api.tvmaze.com/shows/176/episodebynumber?season=" + season + "&number=" + episode;
     httpRequest.open("GET", url, true);
     httpRequest.send();
 }
 
-/**
- * Displays the zip code place given the JSON data
- * @param {string} data JSON data representing place for given zip code
- */
-function displayCast(data){
-    var cast = JSON.parse(data);
-    if(cast.name === "none") {
-        document.getElementById("cast").className = "alert alert-warning";
-        document.getElementById("cast").innerHTML = "No cast matches the show."
-    } else {
-        document.getElementById("cast").className = "alert alert-success";
-         document.getElementById("cast").innerHTML = cast.name +
-        ", " +
-        cast.image +
-        ", " +
-       cast.character;
+function displaySummary(data) {
+    var summary = JSON.parse(data);
+    if (data === "none") {
+        document.getElementById("summary").className = "alert alert-warning";
+        document.getElementById("summary").innerHTML = "There is no episode by the numbers you have chosen."
+    }
+    else {
+        document.getElementById("summary").className = "alert alert-success";
+        document.getElementById("image").src = summary.image.medium;
+        document.getElementById("summary").innerHTML = summary.name + summary.summary;
     }
 }
